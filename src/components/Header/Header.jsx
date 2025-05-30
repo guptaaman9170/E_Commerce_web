2740bd74b43a192a508bab1a26b3f1c562e63c55
Aman gupta <guptaaman9170@gmail.com>
2025-05-30 10:20:23 +0530

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Need something now? Get everything you want — from groceries and meals to everyday essentials — delivered straight to your door in minutes. With fast, reliable service and a wide range of options, our app makes life simpler, one delivery at a time. Just tap, order, and relax — we’ll take care of the rest.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
